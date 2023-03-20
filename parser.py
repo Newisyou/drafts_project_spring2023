58a838259404596be384934cec92162af1312742
Новокрещенова Алёна <91333734+Newisyou@users.noreply.github.com>
2023-03-20 22:36:49 +0500

    1

--- parser.py
+++ parser.py
@@ -2,6 +2,13 @@ import csv
 
 import requests
 
+# collect_openalex собирает данные в формат json и в дальнейшем преобразует их в список.
+# параметры: почта для вежливого пула, фильтер: id project based learning, вывод по 200 записей на каждую страницу,
+# переключение страниц через курсор.
+# (подробная инфа тут: https://docs.openalex.org/how-to-use-the-api/api-overview)
+# Через while переключение страниц. Получает значение нового курсора-перелистывает страницу, считывает,
+# нет перехода - данные закончились
+
 
 def collect_openalex():
     URL_TEMPLATE = 'https://api.openalex.org/works'
@@ -26,13 +33,20 @@ def collect_openalex():
     return all_works
 
 
+# file_writer(data) преобразовывает полученный список в csv файл. Я записала не все параметры, без понятия,
+# понадобятся они или нет. Добавить не трудно. Кодировку стоит прописывать, иначе ругается на непонятные символы
+
+
 def file_writer(data):
     with open('pr_bas_learn.csv', 'w',encoding="utf-8") as file:
         a_pen = csv.writer(file)
-        a_pen.writerow(('id', 'title', 'publication_year', 'authorships', 'concepts', 'referenced_works', 'related_works'))
+        a_pen.writerow(('id', 'title', 'publication_year', 'concepts', 'referenced_works', 'related_works'))
         for works in data:
-            a_pen.writerow((works['id'], works['title'], works['publication_year'], works['authorships'],
-                           works['concepts'],
+            concepts=[]
+            for i in works['concepts']: 
+                concepts.append(i['display_name']) #тут просто решила записать только названия "концепций"
+            a_pen.writerow((works['id'], works['title'], works['publication_year'],
+                           concepts,
                            works['referenced_works'], works['related_works']))
 
 
